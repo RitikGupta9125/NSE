b5b09190c3ed3abce23bf2bf647ff658a7eb14bf
RitikGupta9125 <Ritikgupta912536@gmail.com>
2019-11-20 03:40:32 +0530

    Modified

--- python/graph.py
+++ python/graph.py
@@ -15,7 +15,7 @@ k = [[1,2],[4,8]]
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('basic.html',d = dc.to_json(orient='table') ) # If you replace dc.to_json(orient='table') with k then it work fine
+    return render_template('basic.html',d = dc.reset_index().values ) # If you replace dc.reset_index().values with k then it work fine # If you replace dc.to_json(orient='table') with k then it work fine
 
 if __name__ == '__main__':
     app.run(port = 5000,debug = True )
